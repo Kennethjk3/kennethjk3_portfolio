8d94c579a2ccdf17b39f6710b8c69aad02858cb9
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-10-26 19:42:40 -0400

    first

---
+++ components/Root.jsx
@@ -0,0 +1,35 @@
+var React = require('react')
+var Router = require('react-router')
+var RouteHandler = Router.RouteHandler
+var Header = require('../components/Header.jsx')
+
+var Root = React.createClass({
+  render: function () {
+    this.props.webpackStats = undefined
+    var initialProps = {
+      __html: safeStringify(this.props)
+    }
+
+    return (
+      <html>
+        <head>
+          <title>{this.props.title}</title>
+        </head>
+        <body>
+          <Header {...this.props} />
+          <RouteHandler {...this.props} />
+          <script
+            id='initial-props'
+            type='application/json'
+            dangerouslySetInnerHTML={initialProps} />
+          <script src='bundle.js' />
+        </body>
+      </html>
+    )
+  }
+})
+
+function safeStringify(obj) {
+  return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
+}
+module.exports = Root
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
+var data = require('./data')
+
+module.exports = {
+  entry: './entry.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: __dirname,
+    libraryTarget: 'umd'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.jsx$/, loader: 'babel-loader' }
+    ]
+  },
+
+  plugins: [
+    new StaticSiteGeneratorPlugin('bundle.js', data.routes, data)
+  ]
+}
