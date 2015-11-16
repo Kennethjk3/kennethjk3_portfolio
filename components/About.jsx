7565b901e77c5186f3931e8940ee52d9e1661878
Ken Kier <KenK@Kens-MacBook-Pro.local>
2015-11-16 17:16:46 -0500

    added double column styling to about page

--- components/About.jsx
+++ components/About.jsx
@@ -1,36 +1,45 @@
 var React = require('react')
 
 var About = React.createClass({
-  render: function () {
+  render: function() {
     return (
       <main>
-        <h1>About component</h1>
-          <h1>Hey, whatcha watching?</h1>
-        <p>Oh God, what have I done? Perhaps, but perhaps your civilization is merely the sewer of an even greater society above you! Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them.</p>
-        <p>Soon enough. Yes, if you make it look like an electrical fire. When you do things right, people won't be sure you've done anything at all. You seem malnourished. Are you suffering from intestinal parasites?</p>
-        <h2>Yep, I remember. They came in last at the Olympics, then retired to promote alcoholic beverages!</h2>
-        <p>Quite possible. We live long and are celebrated poopers. You don't know how to do any of those. Throw her in the brig. Isn't it true that you have been paid for your testimony? Noooooo!</p>
-        <ol>
-        <li>Hey, what kinda party is this? There's no booze and only one hooker.</li><li>I don't know what you did, Fry, but once again, you screwed up! Now all the planets are gonna start cracking wise about our mamas.</li><li>Oh yeah, good luck with that.</li>
-        </ol>
+        <div className="about-container">
+          <div className="about-columnleft">
+            <h1>About component</h1>
+            <h1>Hey, whatcha watching?</h1>
+            <p>Oh God, what have I done? Perhaps, but perhaps your civilization is merely the sewer of an even greater society above you! Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them.</p>
+            <p>Soon enough. Yes, if you make it look like an electrical fire. When you do things right, people won't be sure you've done anything at all. You seem malnourished. Are you suffering from intestinal parasites?</p>
+            <h2>Yep, I remember. They came in last at the Olympics, then retired to promote alcoholic beverages!</h2>
+            <p>Quite possible. We live long and are celebrated poopers. You don't know how to do any of those. Throw her in the brig. Isn't it true that you have been paid for your testimony? Noooooo!</p>
+            <ol>
+              <li>Hey, what kinda party is this? There's no booze and only one hooker.</li>
+              <li>I don't know what you did, Fry, but once again, you screwed up! Now all the planets are gonna start cracking wise about our mamas.</li>
+              <li>Oh yeah, good luck with that.</li>
+            </ol>
 
-        <h3>Calculon is gonna kill us and it's all everybody else's fault!</h3>
-        <p>I'm a thing. Oh, I think we should just stay friends. Good man. Nixon's pro-war and pro-family. Kif might!</p>
-        <ul>
-        <li>You're going back for the Countess, aren't you?</li><li>Large bet on myself in round one.</li><li>I feel like I was mauled by Jesus.</li>
-        </ul>
-
-        <p>That's right, baby. I ain't your loverboy Flexo, the guy you love so much. You even love anyone pretending to be him! Bender, you risked your life to save me! I decline the title of Iron Cook and accept the lesser title of Zinc Saucier, which I just made up. Uhh… also, comes with double prize money.</p>
-        <p>Nay, I respect and admire Harold Zoid too much to beat him to death with his own Oscar. It's a T. It goes "tuh". Ummm…to eBay? There, now he's trapped in a book I wrote: a crummy world of plot holes and spelling errors!</p>
-        <p>I had more, but you go ahead. Ah, computer dating. It's like pimping, but you rarely have to use the phrase "upside your head." Whoa a real live robot; or is that some kind of cheesy New Year's costume?</p>
-        <p>Why not indeed! Whoa a real live robot; or is that some kind of cheesy New Year's costume? It's just like the story of the grasshopper and the octopus. All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV. But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car. Is any of this getting through to you?</p>
-        <p>It may comfort you to know that Fry's death took only fifteen seconds, yet the pain was so intense, that it felt to him like fifteen years. And it goes without saying, it caused him to empty his bowels. I meant 'physically'. Look, perhaps you could let me work for a little food? I could clean the floors or paint a fence, or service you sexually?</p>
-        <p>Your best is an idiot! Anyone who laughs is a communist! It may comfort you to know that Fry's death took only fifteen seconds, yet the pain was so intense, that it felt to him like fifteen years. And it goes without saying, it caused him to empty his bowels.</p>
-        <p>For the last time, I don't like lilacs! Your 'first' wife was the one who liked lilacs! Look, everyone wants to be like Germany, but do we really have the pure strength of 'will'? And from now on you're all named Bender Jr.</p>
-        <p>My fellow Earthicans, as I have explained in my book 'Earth in the Balance'', and the much more popular ''Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards. You lived before you met me?!</p>
-        <p>Why would I want to know that? Um, is this the boring, peaceful kind of taking to the streets? Good news, everyone! There's a report on TV with some very bad news! If rubbin' frozen dirt in your crotch is wrong, hey I don't wanna be right.</p>
-        <p>Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that panders to my lust for violence? You guys aren't Santa! You're not even robots. How dare you lie in front of Jesus?</p>
-        <p>Humans dating robots is sick. You people wonder why I'm still single? It's 'cause all the fine robot sisters are dating humans! Meh. Ow, my spirit! Hey! I'm a porno-dealing monster, what do I care what you think?</p>
+            <h3>Calculon is gonna kill us and it's all everybody else's fault!</h3>
+            <p>I'm a thing. Oh, I think we should just stay friends. Good man. Nixon's pro-war and pro-family. Kif might!</p>
+            <ul>
+              <li>You're going back for the Countess, aren't you?</li>
+              <li>Large bet on myself in round one.</li>
+              <li>I feel like I was mauled by Jesus.</li>
+            </ul>
+          </div>
+          <div className="about-columnright">
+            <p>That's right, baby. I ain't your loverboy Flexo, the guy you love so much. You even love anyone pretending to be him! Bender, you risked your life to save me! I decline the title of Iron Cook and accept the lesser title of Zinc Saucier, which I just made up. Uhh… also, comes with double prize money.</p>
+            <p>Nay, I respect and admire Harold Zoid too much to beat him to death with his own Oscar. It's a T. It goes "tuh". Ummm…to eBay? There, now he's trapped in a book I wrote: a crummy world of plot holes and spelling errors!</p>
+            <p>I had more, but you go ahead. Ah, computer dating. It's like pimping, but you rarely have to use the phrase "upside your head." Whoa a real live robot; or is that some kind of cheesy New Year's costume?</p>
+            <p>Why not indeed! Whoa a real live robot; or is that some kind of cheesy New Year's costume? It's just like the story of the grasshopper and the octopus. All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV. But then the winter came, and the grasshopper died, and the octopus ate all his acorns. Also he got a race car. Is any of this getting through to you?</p>
+            <p>It may comfort you to know that Fry's death took only fifteen seconds, yet the pain was so intense, that it felt to him like fifteen years. And it goes without saying, it caused him to empty his bowels. I meant 'physically'. Look, perhaps you could let me work for a little food? I could clean the floors or paint a fence, or service you sexually?</p>
+            <p>Your best is an idiot! Anyone who laughs is a communist! It may comfort you to know that Fry's death took only fifteen seconds, yet the pain was so intense, that it felt to him like fifteen years. And it goes without saying, it caused him to empty his bowels.</p>
+            <p>For the last time, I don't like lilacs! Your 'first' wife was the one who liked lilacs! Look, everyone wants to be like Germany, but do we really have the pure strength of 'will'? And from now on you're all named Bender Jr.</p>
+            <p>My fellow Earthicans, as I have explained in my book 'Earth in the Balance'', and the much more popular ''Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards. You lived before you met me?!</p>
+            <p>Why would I want to know that? Um, is this the boring, peaceful kind of taking to the streets? Good news, everyone! There's a report on TV with some very bad news! If rubbin' frozen dirt in your crotch is wrong, hey I don't wanna be right.</p>
+            <p>Well, thanks to the Internet, I'm now bored with sex. Is there a place on the web that panders to my lust for violence? You guys aren't Santa! You're not even robots. How dare you lie in front of Jesus?</p>
+            <p>Humans dating robots is sick. You people wonder why I'm still single? It's 'cause all the fine robot sisters are dating humans! Meh. Ow, my spirit! Hey! I'm a porno-dealing monster, what do I care what you think?</p>
+          </div>
+        </div>
       </main>
     )
   }
